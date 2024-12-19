cad793a75e90abb5a2a4f7cea67e68e4855d0963
Caio Imbroisi <kaikatsue@gmail.com>
2024-12-19 00:22:03 -0300

    feat: add tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./src/**/*.{vue,js,ts,jsx.tsx}"
-  ],
+  content: ["./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
     extend: {},
   },
   plugins: [],
-}
-
+};
